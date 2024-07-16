b59010e111a24e225794c9ba0cbf6225c34ff4cf
hyunerase <wldn0716@icloud.com>
2024-07-16 11:30:39 +0900

    sisi

--- movieproject/settings.py
+++ movieproject/settings.py
@@ -38,6 +38,29 @@ CORS_ORIGIN_WHITELIST = [
 
 CORS_ALLOW_CREDENTIALS = True
 
+# CORS_ALLOW_METHODS = (  #<-실제 요청에 허용되는 HTTP 동사 리스트
+#     'DELETE',
+#     'GET',
+#     'OPTIONS',
+#     'PATCH',
+#     'POST',
+#     'PUT',
+# )
+
+# CORS_ALLOW_HEADERS = ( <-실제 요청을 할 때 사용될 수 있는 non-standard HTTP 헤더 목록// 현재 기본값
+#     'accept',
+#     'accept-encoding',
+#     'authorization',
+#     'content-type',
+#     'dnt',
+#     'origin',
+#     'user-agent',
+#     'x-csrftoken',
+#     'x-requested-with',
+# )
+
+# APPEND_SLASH = False #<- / 관련 에러 제거
+
 
 # Application definition
 
